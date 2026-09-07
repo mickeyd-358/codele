82bf7a4794b39120e5c1cb77bc80ecef0b8cf7de
mickeyd-358 <michdeft01@gmail.com>
2026-09-08 06:43:33 +1000

    Implement targeted times for user's location in debug game

--- debug.js
+++ debug.js
@@ -1,8 +1,23 @@
-// =========================
-// TODAY'S DATE
-// =========================
+// =====================================
+// TODAY'S DATE (in the user's location)
+// =====================================
 
-const today = new Date().toISOString().split("T")[0];
+function getToday() {
+
+    const date = new Date();
+
+    const year = date.getFullYear();
+
+    const month =
+        String(date.getMonth() + 1).padStart(2, "0");
+
+    const day =
+        String(date.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+}
+
+const today = getToday();
 
 
 // =========================
