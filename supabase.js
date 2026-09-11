f330eef594258ba62ad595adbc112ab39988d907
mickeyd-358 <michdeft01@gmail.com>
2026-09-11 16:50:48 +1000

    Add new file to connect to supabase database

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://apzyuqelpkflhabwzumm.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_cWueYzSb_wb75tch_muxbw_Ir4pWQl5";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
